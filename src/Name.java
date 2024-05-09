9205b2ce85bfb04688c7de47379965662dc206bb
chinmay <cad0980@gmail.com>
2024-05-09 11:48:09 -0500

    new

---
+++ src/Name.java
@@ -0,0 +1,13 @@
+public class Name {
+    public static void main(String[] args) {
+
+
+        char c1 = '\u093F';
+        char c2 = '\u091A';
+        char c3 = '\u0928';
+        char c4 = '\u092E';
+        char c5 = '\u092F';
+
+        System.out.println(""+c1+c2+c3+c4+c5);
+    }
+}
