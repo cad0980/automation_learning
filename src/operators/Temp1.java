84ffb38115863e41b9fb553084a489743c17c681
chinmay <cad0980@gmail.com>
2024-05-13 03:04:08 -0500

    new

---
+++ src/operators/Temp1.java
@@ -0,0 +1,14 @@
+package operators;
+public class Temp1 {
+    public static void main(String[] args) {
+
+        float c1 = 0;
+        float F = ((c1*9)/5)+32;
+        float K = c1+273;
+
+        System.out.println(c1 + " Celsius" + " = " + F + " Fahrenheit");
+        System.out.println(c1 + " Celsius" + " = " + K + " Kelvin");
+
+
+    }
+}
