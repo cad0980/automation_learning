5afe34b055ae2e1fb2335d884557567ab6e4b2e3
chinmay <cad0980@gmail.com>
2024-05-13 04:16:27 -0500

    new

---
+++ src/operators/Year.java
@@ -0,0 +1,13 @@
+package operators;
+public class Year {
+    public static void main(String[] args) {
+
+        double min = 525920;
+        double c = (0.000694); //conversion of 1 min to day
+        double days = min * c;
+        double year = days/365;
+        System.out.println(min);
+        System.out.println(days);
+        System.out.println(year);
+    }
+}
