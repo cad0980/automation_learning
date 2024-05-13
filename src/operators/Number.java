85e209720b737842d0433050f330e71b60ccfb6c
chinmay <cad0980@gmail.com>
2024-05-13 04:02:02 -0500

    new

---
+++ src/operators/Number.java
@@ -0,0 +1,16 @@
+package operators;
+public class Number {
+    public static void main(String[] args) {
+
+        double i= 5;
+        double sq = i*i;
+        double cu = sq*i;
+        double fo = cu*i;
+
+        System.out.println("Square = " + sq);
+        System.out.println("Cube = " + cu);
+        System.out.println("Fourth power = " + fo);
+
+
+    }
+}
