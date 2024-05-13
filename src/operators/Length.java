7221e41772a5114dc17728309e9600464dcbeaa7
chinmay <cad0980@gmail.com>
2024-05-13 03:21:17 -0500

    new

---
+++ src/operators/Length.java
@@ -0,0 +1,15 @@
+package operators;
+public class Length {
+    public static void main(String[] args) {
+
+       double i= 5;
+       double c = 1/ 39.97;
+       double m = i * c;
+
+        System.out.println("The length is " + i + " inches");
+        System.out.println(" 1 inch = " + c + " meters");
+        System.out.println("Therefore 5 inches = " + m + " meters");
+
+
+    }
+}
