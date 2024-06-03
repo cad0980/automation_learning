160b12730546f31015414b9a8e0ea8f3e769d8f8
chinmay <cad0980@gmail.com>
2024-06-02 21:16:59 -0500

    updates

---
+++ src/loopdemo/Infinite2.java
@@ -0,0 +1,12 @@
+package loopdemo;
+
+public class Infinite2 {
+    public static void main(String[] args) {
+        //for(;;)
+        for (int i=0; i<1;)
+        {
+            System.out.println("Hello");
+        }
+    }
+
+    }
