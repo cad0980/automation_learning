9920e52d5ee9fc17a12be5efede3287dd43fadf8
chinmay <cad0980@gmail.com>
2024-06-02 21:00:57 -0500

    updates

---
+++ src/loopdemo/Fib1.java
@@ -0,0 +1,14 @@
+package loopdemo;
+
+public class Fib1 {
+    public static void main(String[] args) {
+        int n = 10, n1= 0, n2=1, n3;
+
+        for(int i=1; i<=n; i++){
+            System.out.print(n1+", ");
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+    }
+  }
