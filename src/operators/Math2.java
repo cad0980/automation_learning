4e31b1c9ff34b550dea19997f26b91a785f792d4
chinmay <cad0980@gmail.com>
2024-06-03 22:09:59 -0500

    updates

---
+++ src/operators/Math2.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class Math2 {
+    public static void main(String[] args) {
+    int a = 1, b =2, c=6, d=7;
+    // operators +,-,*
+
+        int s = ((c-a)*b)+d;
+        System.out.println("The answer of the equation is s = "+s);
+    }
+        }
