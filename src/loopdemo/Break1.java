9e6ecbf67ab112a0a44ffec9dc81f848cc0f76df
chinmay <cad0980@gmail.com>
2024-06-03 21:59:14 -0500

    updates

---
+++ src/loopdemo/Break1.java
@@ -0,0 +1,17 @@
+package loopdemo;
+
+import java.util.*;
+
+public class Break1 {
+    public static void main(String[] args) {
+    while (true){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int in = scan.nextInt();
+        if (in < 0){
+            break;
+        }
+
+    }
+    }
+    }
